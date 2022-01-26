b553e9166ff3d1c36b22c5ad35312e2a63385eec
418cat <benjaminbadina@gmail.com>
2022-01-26 18:42:26 +0100

    just a few things more

--- app/src/main/java/com/example/systemmonitor/MainActivity.java
+++ app/src/main/java/com/example/systemmonitor/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.systemmonitor;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import com.jcraft.jsch.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void enableDisable(View v){
+    public void sshConnect(View v){
         Button btn = (Button)v;
-        Button btn2;
-        if(btn.getId() == R.id.button){
-            btn2 = (Button)findViewById(R.id.button2);
-        } else {
-            btn2 = (Button)findViewById(R.id.button);
-        }
-        btn.setEnabled(false);
-        btn.setText("désactivé");
-        btn2.setEnabled(true);
-        btn2.setText("activé");
+        String[] infos = {"", "", "", ""};
+        infos[0] = String.valueOf((TextView)findViewById(R.id.username));
+        infos[1] = String.valueOf((TextView)findViewById(R.id.hostname));
+        infos[2] = String.valueOf((TextView)findViewById(R.id.port));
+        infos[3] = String.valueOf((TextView)findViewById(R.id.password));
+
+
     }
 }
