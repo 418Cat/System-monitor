2f17550f2ff346789aeb3ec49ff78de872a88d27
418cat <benjaminbadina@gmail.com>
2022-01-27 19:22:46 +0100

    Started adding tabs & translation but somehow when I press a button the app crashes, gotta fix that

--- app/src/main/java/com/example/systemmonitor/MainActivity.java
+++ app/src/main/java/com/example/systemmonitor/MainActivity.java
@@ -1,35 +1,49 @@
 package com.example.systemmonitor;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaCas;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import com.jcraft.jsch.*;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
     public SSH connection;
+    public String[][] translations = {{"English", "Username", "Hostname", "Port", "Password", "Connect", "Command", "Execute"}, {"Français", "Nom d'utilisateur", "Hôte", "Port", "Mot de passe", "Connection", "Commande", "Executer"}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setLanguages("Français");
+    }
+
+    public void setLanguages(String language){
+        String[] translate = {};
+        for(String[] i:translations){
+            if(i[0] == language){
+                translate = i;
+                break;
+            }
+        }
+        ((TextView)findViewById(R.id.username)).setHint(translate[1]);
+        ((TextView)findViewById(R.id.hostname)).setHint(translate[2]);
+        ((TextView)findViewById(R.id.port)).setHint(translate[3]);
+        ((TextView)findViewById(R.id.password)).setHint(translate[4]);
+        ((TextView)findViewById(R.id.connect_button)).setText(translate[5]);
+        ((TextView)findViewById(R.id.input_text)).setHint(translate[6]);
+        ((TextView)findViewById(R.id.execute_button)).setText(translate[7]);
     }
 
     public void ssh(View v){
         connection = new SSH();
-        String[] infos = {((TextView)findViewById(R.id.username)).getText().toString(), ((TextView)findViewById(R.id.hostname)).getText().toString(), ((TextView)findViewById(R.id.port)).getText().toString(), ((TextView)findViewById(R.id.password)).getText().toString()};
-        TextView output = (TextView) findViewById(R.id.output);
-        connection.infos = infos;
-        connection.input = (TextView)findViewById(R.id.input);
-        connection.output = (TextView)findViewById(R.id.output);
+        String[] userInfo = {((TextView)findViewById(R.id.username)).getText().toString(), ((TextView)findViewById(R.id.hostname)).getText().toString(), ((TextView)findViewById(R.id.port)).getText().toString(), ((TextView)findViewById(R.id.password)).getText().toString()};
+        connection.userInfo = userInfo;
+        connection.input = (TextView)findViewById(R.id.input_text);
+        connection.output = (TextView)findViewById(R.id.text_output);
         connection.ssh();
     }
 
@@ -41,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
 class SSH extends Thread{
 
-    public String[] infos;
+    public String[] userInfo;
     public TextView output;
     public TextView input;
 
@@ -52,12 +66,12 @@ class SSH extends Thread{
         public void run(){
             try {
 
-                (session = new JSch().getSession(infos[0], infos[1], Integer.parseInt(infos[2]))).setPassword(infos[3]);
+                (session = new JSch().getSession(userInfo[0], userInfo[1], Integer.parseInt(userInfo[2]))).setPassword(userInfo[3]);
                 session.setConfig("StrictHostKeyChecking", "no");
                 session.connect();
 
                 Log.d("SSH", "CONNECTION SUCCESS");
-                output.setText(infos[0] + " connecté en ssh à " + infos[1]);
+                output.setText(userInfo[0] + " connecté en ssh à " + userInfo[1]);
 
             } catch (Exception e) {
                 Log.d("SSH", "CONNECTION FAILURE: " + e.toString());
